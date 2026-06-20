9fda3196c1da89e04d668040ef522c3bcd5f11ee
Tebogo <herroldmoremi@hotmail.com>
2026-06-20 20:45:19 +0200

    added the port

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-import express from 'express';
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express(); //initializing express app
+const PORT = 5000; //initializing the port
+
+//initializing the bodyParser middleware
+app.use(bodyParser.json());
+//application listen to upcoming requests
+app.listen(PORT,()=>console.log(`server running on port:http://localhost:${PORT}`));
